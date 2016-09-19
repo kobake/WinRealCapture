e4fd00f18ded6b60929908466265cc8ab7f215fd
kobake <kobake@users.sourceforge.net>
2016-09-19 23:20:40 +0900

    wip キーフック検証中

--- EasyCapture/Program.cs
+++ EasyCapture/Program.cs
@@ -1,15 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EasyCapture
 {
 	class Program
 	{
-		static void Main(string[] args)
+		const int WH_KEYBOARD_LL = 13;
+		const int WM_KEYDOWN = 0x0100;
+
+		static LowLevelKeyboardProc _proc = HookCallback;
+		static IntPtr _hookID = IntPtr.Zero;
+
+		[DllImport("user32.dll")]
+		static extern short GetAsyncKeyState(Keys vKey);
+
+		[DllImport("user32.dll")]
+		static extern short GetKeyState(Keys vKey);
+
+		const int VK_F2 = 0x71;
+		const int VK_F10 = 0x79;        // F10 キー
+		const int VK_F11 = 0x7A;        // F11 キー
+		const int VK_MULTIPLY = 0x6A;   // *　キー
+		const int VK_CONTROL = 0x11;
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		static extern IntPtr SetWindowsHookEx(
+			int idHook,
+			LowLevelKeyboardProc lpfn,
+			IntPtr hMod,
+			uint dwThreadId
+		);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		static extern IntPtr CallNextHookEx(
+			IntPtr hhk,
+			int nCode,
+			IntPtr wParam,
+			IntPtr lParam
+		);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		static extern IntPtr GetModuleHandle(string lpModuleName);
+
+		private static IntPtr SetHook(LowLevelKeyboardProc proc)
 		{
+			using (Process curProcess = Process.GetCurrentProcess())
+			using (ProcessModule curModule = curProcess.MainModule)
+			{
+				return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+			}
+		}
+		private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+		private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+		{
+			if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+			{
+				int vkCode = Marshal.ReadInt32(lParam);
+				Console.WriteLine((Keys)vkCode);
+			}
+			return CallNextHookEx(_hookID, nCode, wParam, lParam);
+		}
+
+		static void Main()
+		{
+			var doWork = Task.Run(() =>
+			{
+				try
+				{
+					Thread.Sleep(Timeout.Infinite);
+				}
+				catch (ThreadInterruptedException)
+				{
+				}
+				Application.Exit(); // Quick exit for demonstration only.
+			});
+
+			_hookID = SetHook(_proc);
+
+			Application.Run();
+
+			UnhookWindowsHookEx(_hookID);
+		}
+		static void Main2(string[] args)
+		{
+			
+			while (true)
+			{
+				Thread.Sleep(1000);
+
+				if (true)
+				{
+					short s = GetAsyncKeyState(Keys.F2);
+					short s1 = GetAsyncKeyState(Keys.ControlKey);
+					short s2 = GetKeyState(Keys.ControlKey);
+					if (s != 0)
+					{
+						Console.WriteLine("F2 pushed: {0}", Convert.ToString(s, 2));
+						Console.WriteLine("Ctrl_1: {0}", Convert.ToString(s1, 2));
+						Console.WriteLine("Ctrl_2: {0}", Convert.ToString(s2, 2));
+						Console.WriteLine("");
+					}
+				}
+				
+			}
 		}
 	}
 }
